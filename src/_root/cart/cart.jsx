1217d43074e95af53d784c582edb5d71b9808f23
upraisertech <akoredesalaudeen54@gmail.com>
2024-04-08 22:29:55 +0100

    cart page

--- src/_root/cart/cart.jsx
+++ src/_root/cart/cart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import product_1 from "../../assets/images/product_1.svg";
 
 export default function Cart() {
   return (
@@ -10,7 +11,23 @@ export default function Cart() {
           <div>Quantity</div>
           <div>Subtotal</div>
         </div>
-        <div className="bg-[yellow]"></div>
+        <div className="flex flex-row mt-12 gap-6 items-center justify-start">
+          <div className="flex flex-row gap-6 text-gray-400 items-center justify-start">
+            <img src={product_1} alt="" />
+            <div>MIRAGE MR-AT172 285/65</div>
+            <div>Rs. 50,000.00</div>
+          </div>
+          <input
+            className="w-12 h-8 rounded-md border-2 text-center"
+            type="number"
+            placeholder="1"
+            min="1"
+          />
+          <div>Rs. 50,000.00</div>
+          <div className="text-[20px] text-[#FBEBB5] font-bold cursor-pointer">
+            <ion-icon name="trash-outline"></ion-icon>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col p-5 h-[20em] w-[30em] items-center justify-start gap-3 bg-[#FFF9E5] rounded-md">
         <div className="text-[25px] font-bold">Cart Totals</div>
@@ -22,7 +39,9 @@ export default function Cart() {
           <div className="text-black font-bold">Total</div>
           <div className="text-[#B88E2F]">Rs. 50,000.00</div>
         </div>
-        <button className="border border-black mt-9 text-black hover:text-white bg-none hover:bg-black font-bold px-8 py-3 rounded-lg">Check Out</button>
+        <button className="border border-black mt-9 text-black hover:text-white bg-none hover:bg-black font-bold px-8 py-3 rounded-lg">
+          Check Out
+        </button>
       </div>
     </div>
   );
