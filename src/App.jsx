b47938fe180aee6517245ee4628697e04f1c3dec
upraisertech <akoredesalaudeen54@gmail.com>
2024-04-23 22:02:46 +0100

    add home

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./Auths/signin";
 import SignUp from "./Auths/signup";
-import Home from "./_root/home";
+import Home from "./_root/home/home";
 import Cart from "./_root/cart/index";
 import CheckOut from "./_root/CheckOut/index";
-import Shop from "./_root/shop/index"
+import Shop from "./_root/shop/index";
+import ScrollToTop from "./BackToTop/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<CheckOut />} />
-        <Route path="/shop" element={<Shop/>} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </>
   );
