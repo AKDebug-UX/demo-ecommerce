0a22f7de5032070292a8a9f791b084c7f8cc4e59
Unclesam <adeyemis958@gmail.com>
2024-04-24 04:47:53 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ import Home from "./_root/home/home";
 import Cart from "./_root/cart/index";
 import CheckOut from "./_root/CheckOut/index";
 import Shop from "./_root/shop/index";
-<<<<<<< HEAD
 import ProductDetail from "./_root/productDetail/index";
-=======
 import ScrollToTop from "./BackToTop/ScrollToTop";
->>>>>>> b47938fe180aee6517245ee4628697e04f1c3dec
 
 function App() {
   return (
@@ -23,10 +20,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<CheckOut />} />
         <Route path="/shop" element={<Shop />} />
-<<<<<<< HEAD
         <Route path="/products/:productId" element={<ProductDetail />} />
-=======
->>>>>>> b47938fe180aee6517245ee4628697e04f1c3dec
       </Routes>
     </>
   );
